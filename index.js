4582f835671783c523e002ffb539cc7346ebfde0
Zach Van Uum <zachvanuum@gmail.com>
2018-01-30 21:47:42 -0700

    Reformatting and adding a config file

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict';
 
-
 process.env.DEBUG = 'actions-on-google:*';
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
+const fetch = require('node-fetch');
 
-const https = require('https');
-const host = 'https://na1.api.riotgames.com';
-const champions = '/lol/static-data/v3/champions';
-const apiKey = 'RGAPI-51918406-c03f-4c8d-bb93-42d874980db6';
+// const host = 'https://na1.api.riotgames.com';
+// const champions = '/lol/static-data/v3/champions';
+// const apiKey = 'RGAPI-8105d3d2-3655-41ec-9845-387c82a486d5';
+const config = require('./config.json');
 
-const googleAssistantRequest = 'google';
 const actions = {
-    GET_CHAMPION: 'get-champion'
+    WELCOME: 'input.welcome',
+    DEFAULT: 'default',
+    GET_CHAMPION: 'get-champion',
+    GET_RANDOM_CHAMPION: 'get-random-champion'
 };
 
 const parameters = {
@@ -23,51 +25,14 @@ const contexts = {
 
 };
 
-function processRequest(request, response) {
-    let action = request.body.result.action;
-    let parameters = request.body.result.parameters;
-    let inputContexts = request.body.result.contexts;
-    let requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
-
-    console.log(request.body);
-    console.log('ACTION:', action);
-
+function LeagueInfoAssistant(request, response) {
     const app = new DialogflowApp({ request, response });
-    const actionHandlers = {
-        'input.welcome': () => {
-            if (requestSource === googleAssistantRequest) {
-                sendGoogleResponse('Hello, Welcome to my Dialogflow agent!');
-            } else {
-                sendResponse('Hello, Welcome to my Dialogflow agent!');
-            }
-        },
-        'default': () => {
-            if (requestSource === googleAssistantRequest) {
-                sendGoogleResponse({
-                    speech: 'This message is from Dialogflow\'s Cloud Functions for Firebase editor!',
-                    text: 'This is from Dialogflow\'s Cloud Functions for Firebase editor! :-)'
-                });
-            } else {
-                sendResponse({
-                    speech: 'This message is from Dialogflow\'s Cloud Functions for Firebase editor!',
-                    text: 'This is from Dialogflow\'s Cloud Functions for Firebase editor! :-)'
-                });
-            }
-        },
-        [actions.GET_CHAMPION]: () => {
-            if (requestSource === googleAssistantRequest) {
-                sendGoogleResponse({
-                    speech: 'fvlsdbfs',
-                    text: 'fvlsdbfs'
-                });
-            } else {
-                sendResponse({
-                    speech: 'fvlsdbfs',
-                    text: 'fvlsdbfs'
-                });
-            }
-        }
-    };
+    const actionHandlers = {};
+    actionHandlers[actions.WELCOME] = welcomeResponse;
+    actionHandlers[actions.DEFAULT] = defaultResponse;
+    actionHandlers[actions.GET_CHAMPION] = getChampion;
+    actionHandlers[actions.GET_RANDOM_CHAMPION] = getRandomChampion;
+    const { action, parameters, contexts } = request.body.result;
 
     if (!actionHandlers[action]) {
         action = 'default';
@@ -75,102 +40,59 @@ function processRequest(request, response) {
 
     actionHandlers[action]();
 
-
-
-    // Function to send correctly formatted Google Assistant responses to Dialogflow which are then sent to the user
-    function sendGoogleResponse(responseToUser) {
-        if (typeof responseToUser === 'string') {
-            app.ask(responseToUser); // Google Assistant response
-        } else {
-            // If speech or displayText is defined use it to respond
-            let googleResponse = app.buildRichResponse().addSimpleResponse({
-                speech: responseToUser.speech || responseToUser.displayText,
-                displayText: responseToUser.displayText || responseToUser.speech
-            });
-            // Optional: Overwrite previous response with rich response
-            if (responseToUser.googleRichResponse) {
-                googleResponse = responseToUser.googleRichResponse;
-            }
-            // Optional: add contexts (https://dialogflow.com/docs/contexts)
-            if (responseToUser.googleOutputContexts) {
-                app.setContext(...responseToUser.googleOutputContexts);
-            }
-
-            console.log('Response to Dialogflow (AoG): ' + JSON.stringify(googleResponse));
-            app.ask(googleResponse); // Send response to Dialogflow and Google Assistant
-        }
+    function welcomeResponse() {
+        sendResponse('Hello, Welcome to my Dialogflow agent!');
     }
 
-    // Function to send correctly formatted responses to Dialogflow which are then sent to the user
+    function defaultResponse() {
+        sendResponse({
+            speech: 'This message is from Dialogflow\'s Cloud Functions for Firebase editor!',
+            text: 'This is from Dialogflow\'s Cloud Functions for Firebase editor! :-)'
+        });
+    }
+
+    function getChampion() {
+        sendResponse({
+            speech: 'fvlsdbfs',
+            text: 'fvlsdbfs'
+        });
+    }
+
+    function getRandomChampion() {
+        getChampionsFromRiot()
+            .then(champions => {
+                const randomIndex = Math.floor(Math.random() * champions.length);
+                sendResponse(champions[randomIndex].name);
+            })
+            .catch(err => {
+                console.error(err);
+                sendResponse('Sorry, something went wrong. Try again in a bit.');
+            });
+    }
+
     function sendResponse(responseToUser) {
-        // if the response is a string send it as a response to the user
+        let responseJson = {};
+
         if (typeof responseToUser === 'string') {
-            let responseJson = {};
-            responseJson.speech = responseToUser; // spoken response
-            responseJson.displayText = responseToUser; // displayed response
-            response.json(responseJson); // Send response to Dialogflow
+            responseJson.speech = responseToUser;
+            responseJson.displayText = responseToUser;
         } else {
-            // If the response to the user includes rich responses or contexts send them to Dialogflow
-            let responseJson = {};
-            // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
             responseJson.speech = responseToUser.speech || responseToUser.displayText;
             responseJson.displayText = responseToUser.displayText || responseToUser.speech;
-            // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
             responseJson.data = responseToUser.data;
-            // Optional: add contexts (https://dialogflow.com/docs/contexts)
             responseJson.contextOut = responseToUser.outputContexts;
-
-            console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
-            response.json(responseJson); // Send response to Dialogflow
         }
+
+        console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
+        response.json(responseJson);
     }
 }
 
-exports.leagueInfo = functions.https.onRequest((request, response) => {
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-    processRequest(request, response);
-});
+function getChampionsFromRiot() { 
+    console.log(`Sending request: ${config.host}${config.champions}?api_key=${config.apiKey}`);
+    return fetch(`${config.host}${config.champions}?api_key=${config.apiKey}`)
+        .then(res => res.json())
+        .then(res => Object.keys(res.data).map((key) => res.data[key]));
+}
 
-// exports.leagueInfo = functions.https.onRequest((req, res) => {
-//     getChampions()
-//         .then(champions => {
-//             const randomIndex = Math.floor(Math.random() * champions.length);
-
-//             res.setHeader('Content-Type', 'application/json');
-//             res.send(JSON.stringify({
-//                 "speech": champions[randomIndex].name,
-//                 "displayText": champions[randomIndex].name
-//             }));
-//         })
-//         .catch(err => {
-//             console.error(err);
-//             res.status(500).send();
-//         });
-// });
-
-// function getChampions() {
-//     return new Promise((resolve, reject) => {
-//         let path = `${champions}?api_key=${apiKey}`;
-
-//         console.log('API Request: ' + `${host}${champions}?api_key=${apiKey}`);
-
-//         https.get(`${host}${champions}?api_key=${apiKey}`, (res) => {
-//             let body = '';
-
-//             res.on('data', (d) => { body += d; });
-//             res.on('end', () => {
-//                 let res = JSON.parse(body);
-
-//                 const values = Object.keys(res.data).map(function(key) {
-//                     return res.data[key];
-//                 });
-
-//                 resolve(values);
-//             });
-//             res.on('error', (error) => {
-//                 reject(error);
-//             });
-//         });
-//     });
-// }
+exports.leagueInfo = functions.https.onRequest(LeagueInfoAssistant);
